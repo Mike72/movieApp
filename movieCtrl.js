57824aa96e94e8c81fc3049d2d7f176f038c303d
Mike72 <mike.zoltan72@hotmail.com>
2017-11-16 11:30:31 +0200

    div=container class= row

--- movieCtrl.js
+++ movieCtrl.js
@@ -1,15 +1,16 @@
 app.controller("movieCtrl", function($scope){
 
 //Actor Constractor
-function Actor(fname,lname,image){
+function Actor(fname,lname,image,href){
     this.fname = fname;
     this.lname = lname;
     this.image = image;
+    this.href = href;
 }
 
-$scope.actors = [new Actor("Daisy","Ridley","https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzMDk3MjI4OF5BMl5BanBnXkFtZTgwMzQxMDY5NjE@._V1_UY317_CR20,0,214,317_AL_.jpg"),
-                new Actor("John","Boyega","https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzNTgzNTI1MF5BMl5BanBnXkFtZTgwMjc2OTM5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg"),
-                new Actor("Mark","Hamill","https://images-na.ssl-images-amazon.com/images/M/MV5BMTY3Njc5ODc4OV5BMl5BanBnXkFtZTYwNjY5MTU0._V1_UX214_CR0,0,214,317_AL_.jpg")
+$scope.actors = [new Actor("Daisy"," Ridley","https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzMDk3MjI4OF5BMl5BanBnXkFtZTgwMzQxMDY5NjE@._V1_UY317_CR20,0,214,317_AL_.jpg","http://www.imdb.com/name/nm5397459/?ref_=tt_ov_st_sm"),
+                new Actor("John"," Boyega","https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzNTgzNTI1MF5BMl5BanBnXkFtZTgwMjc2OTM5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg","http://www.imdb.com/name/nm3915784/?ref_=tt_ov_st_sm"),
+                new Actor("Mark"," Hamill","https://images-na.ssl-images-amazon.com/images/M/MV5BMTY3Njc5ODc4OV5BMl5BanBnXkFtZTYwNjY5MTU0._V1_UX214_CR0,0,214,317_AL_.jpg","http://www.imdb.com/name/nm0000434/?ref_=tt_ov_st_sm")
 
 
 ];
