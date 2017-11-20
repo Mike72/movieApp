5c5d7bee8bbc82ca8b0a1321061e8b42a32f8106
Mike72 <mike.zoltan72@hotmail.com>
2017-11-20 11:08:18 +0200

    code in the movieCtrl.js

--- movieCtrl.js
+++ movieCtrl.js
@@ -1,4 +1,4 @@
-app.controller("movieCtrl", function($scope){
+app.controller("movieCtrl", function($scope ,$http){
 
 //Actor Constractor
 function Actor(fname,lname,image,href){
@@ -7,7 +7,17 @@ function Actor(fname,lname,image,href){
     this.image = image;
     this.href = href;
 }
+$scope.actors = [];
 
+  $http.get("actors.json").then(function mySuccess(response) {
+    for (var i = 0; i < response.data.length; i++) {
+      $scope.actors.push(new Actor(response.data[i].fname, response.data[i].lname, 
+        response.data[i].image, response.data[i].href))  
+    }
+    //alert("success" + JSON.stringify(response.status));
+  }, function myError(response) {
+    alert("error" + JSON.stringify(response.status));
+  })
 /*$scope.actors = [new Actor("Daisy","Ridley","https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzMDk3MjI4OF5BMl5BanBnXkFtZTgwMzQxMDY5NjE@._V1_UY317_CR20,0,214,317_AL_.jpg","http://www.imdb.com/name/nm5397459/?ref_=tt_ov_st_sm"),
                 new Actor("John","Boyega","https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzNTgzNTI1MF5BMl5BanBnXkFtZTgwMjc2OTM5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg","http://www.imdb.com/name/nm3915784/?ref_=tt_ov_st_sm"),
                 new Actor("Mark","Hamill","https://images-na.ssl-images-amazon.com/images/M/MV5BMTY3Njc5ODc4OV5BMl5BanBnXkFtZTYwNjY5MTU0._V1_UX214_CR0,0,214,317_AL_.jpg","http://www.imdb.com/name/nm0000434/?ref_=tt_ov_st_sm"),
